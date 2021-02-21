1143577579a2d60924bdbd9ef189b9bace11c560
Robinson Labourdette <robinson@inoft.com>
2021-02-21 21:25:31 +0100

    Cleanup

--- audio_engine/src/exporter.rs
+++ audio_engine/src/exporter.rs
@@ -110,70 +110,6 @@ pub async fn get_upload_url(filename: String, filesize: usize) -> Result<Option<
     Ok(jsonified_output_data)
 }
 
-// flac_song: &std::fs::File
-pub async fn from_flac_to_mp3() -> Vec<u8> {
-    let file = File::open("F:/Sons utiles/compressed-music.flac").expect("Error opening file");
-    let mut flac_reader = FlacReader::new(file).expect("FlacReader error");
-    let mut lame = Lame::new().expect("Coudn't create Lame");
-
-    lame.set_channels(1).expect("Couldn't set num channels");
-    /*lame.set_sample_rate(flac_reader.streaminfo().sample_rate as _)
-        .expect("Couldn't set up sample rate");
-     */
-    lame.set_sample_rate(16000 as u32).expect("Couldn't set up sample rate");
-    lame.set_channels(1).expect("Coudn't set up channels");
-    lame.set_kilobitrate(48).expect("Coudn't set up kilobitrate");
-
-    let mut left_samples: Vec<i16> = Vec::new();
-    let mut right_samples: Vec<i16> = Vec::new();
-
-    let mut frame_reader = flac_reader.blocks();
-    loop {
-        let frame: Vec<i32> = Vec::new();
-        let result = frame_reader
-            .read_next_or_eof(frame)
-            .expect("error to read frame");
-
-        match result {
-            None => {
-                break;
-            }
-            Some(block) => {
-                let iter = block.stereo_samples();
-                for (left, right) in iter {
-                    left_samples.push(left as _);
-                    right_samples.push(right as _);
-                }
-            }
-        };
-    }
-
-    let num_samples = left_samples.len() as f64;
-    let mp3_buffer_size = (1.25 * num_samples + 7200.0) as usize;
-
-    let mut mp3_buffer = vec![0; mp3_buffer_size];
-
-    lame.set_quality(4).expect("Set quality error");
-    lame.init_params().expect("init parametrs error");
-
-    println!("lame start");
-    let start = Instant::now();
-    let _ = lame.encode(
-        left_samples.as_slice(),
-        right_samples.as_slice(),
-        mp3_buffer.as_mut_slice(),
-    );
-    println!("\nFinished lame.\n  --execution_time:{}ms", start.elapsed().as_millis());
-
-    // tokio::spawn(get_upload_url());
-    /*let mut rt = tokio::runtime::Runtime::new().unwrap();
-    let future = get_upload_url();
-    rt.block_on(future);
-     */
-
-    mp3_buffer
-}
-
 pub fn from_samples_to_mono_mp3(samples: Vec<i16>, target_spec: &ReceivedTargetSpec) -> Vec<u8> {
     let start = Instant::now();
 
@@ -198,11 +134,7 @@ pub fn from_samples_to_mono_mp3(samples: Vec<i16>, target_spec: &ReceivedTargetS
     //  zeros, which we remove right after encoding with lame, by using a simple loop with a step size.
 
     let samples_slice = samples.as_slice();
-    let _ = lame.encode(
-        samples_slice,
-        samples_slice,
-        mp3_buffer.as_mut_slice(),
-    );
+    lame.encode(samples_slice, samples_slice, mp3_buffer.as_mut_slice());
 
     let precision_10ms_samples_step = (target_spec.sample_rate / 100) as usize;
     // We need a custom sample_step to iterate over the mp3_buffer, because if we iterate over every single sample
@@ -224,18 +156,6 @@ pub fn from_samples_to_mono_mp3(samples: Vec<i16>, target_spec: &ReceivedTargetS
         // samples_step from it would make its value go below zero. If this happened, the index would jump to usize::MAX
     }
     println!("Finished removing of trailing empty data from the mp3_buffer.\n  --execution_time:{}ms", start_removing_trailing_empty_data.elapsed().as_millis());
-
-    /*tokio::runtime::Handle::try_current()
-        // tokio::spawn(get_upload_url());
-    }*/
-
-    /*
-    let mut rt2 = tokio::runtime::Runtime::new().unwrap();
-    let future = get_upload_url();
-    rt2.block_on(future);
-     */
-    // tokio::spawn(get_upload_url());
-
     println!("\nFinished MP3 conversion using Lame.\n  --execution_time:{}ms", start.elapsed().as_millis());
     mp3_buffer
 }
