c9ccba3dedd2095eef92295cc4d1c73c7071d6c9
Robinson Labourdette <robinson@inoft.com>
2021-04-03 21:52:04 +0200

    Cleaned up package_nightly_release script

--- package_as_nightly_release_layer_v2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from serverlesspack.cli import package_api
-
-ab = os.path.relpath('../inoft_vocal_framework')
-package_api(target_os='linux', config_filepath='serverlesspack.config.yaml', verbose=False)
