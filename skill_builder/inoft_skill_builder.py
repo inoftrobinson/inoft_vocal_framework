c1d52ddeccbe42d36cad2680a758f59ac73abfce
Robinson Labourdette <robinson@inoft.com>
2021-04-14 11:52:09 +0200

    Removed performances test
    Tested compared to the old NestedObjectDict. The Pydantic integration is around 8 times faster.

--- skill_builder/inoft_skill_builder.py
+++ skill_builder/inoft_skill_builder.py
@@ -409,12 +409,7 @@ class InoftSkill:
             from inoft_vocal_framework.messages import ERROR_PLATFORM_NOT_SUPPORTED
             raise Exception(ERROR_PLATFORM_NOT_SUPPORTED)
 
-        import time
-        start_event_load = time.time()
-        for i in range(1000):
-            self.handler_input.load_event(event=event)
-        elapsed_event_load = time.time() - start_event_load
-        print(f"Took {elapsed_event_load}s to load the event 1000 times")
+        self.handler_input.load_event(event=event)
 
         from inoft_vocal_framework.user_data.user_data import UserData
         self.handler_input._user_data = UserData(user_id=self.handler_input.persistent_user_id)
